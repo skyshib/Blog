80f5557fc3158379ec242de2f4378c93c8cbbb4d
skyshib <skyshib@gmail.com>
2019-12-01 14:57:07 -0800

    add association between user and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_01_212133) do
+ActiveRecord::Schema.define(version: 2019_12_01_224946) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
