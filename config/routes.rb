84cef8384684b65aa8a0a7ac295d3ca1c293dd3a
skyshib <skyshib@gmail.com>
2020-04-03 15:24:30 -0700

    test commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'articles#index'
   resources :articles
+  get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
   get 'login', to: 'sessions#new'
